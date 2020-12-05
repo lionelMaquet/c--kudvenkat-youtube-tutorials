a96fcbb9ae1fb18d86511ad7cf0b31ab0c820647
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-05 16:32:22 +0100

    Part 5 finished

--- kudvenkat_tutorials/Part_5_Common_operators_project/Program.cs
+++ kudvenkat_tutorials/Part_5_Common_operators_project/Program.cs
@@ -5,7 +5,35 @@ class Program
 {
     static void Main()
     {
+        // Assignment operator ( = )
+        int i = 10;
+        bool b = true;
+
+        // Arithmetic operators ( +, -, *, /, % )
+        i = 4 + 2; // 6
+        i = 4 - 2; // 2
+        i = 4 * 2; // 8
+        i = 4 / 2; // 2
+        i = 5 % 2; // Reminder operator -> 1
+
+        // Comparison operators ( ==, !=, >, >=, <, <= ) 
+        b = 4 == 2; // false
+        b = 4 != 2; // true
+        b = 2 > 2; // false
+        b = 2 >= 2; // true
+        b = 4 < 2; // false
+        b = 4 <= 2; // false
+
+        // Conditional operators ( &&, || )
+        b = (2 == 2 && 4 == 2); // false
+        b = (2 == 2 || 4 == 2); // true
+
+        // Ternary operator ( ? : )
+        i = (2 == 2) ? 4 : 3; // i gets 4 because 2 == 2 is true
+        i = (4 == 2) ? 4 : 3; // i gets 3 because 4 == 2 is false
+        b = (4 == 2) ? false : true; // b gets true because 4 == 2 is false
         
+        // Null Coalescing Operator ( ?? ) is seen later
     }
 }
 
