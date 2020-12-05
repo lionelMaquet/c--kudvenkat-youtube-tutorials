f880d59fee5930bcb81d65fe0323f2c327862cf0
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-05 15:10:44 +0100

    PART 2 finished

---
+++ kudvenkat_tutorials/Part_2_project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your name");
+        string Username = Console.ReadLine();
+        //Console.WriteLine("Hello " + Username); // Concaténation 
+        Console.WriteLine("Please enter your age");
+        string Age = Console.ReadLine();
+        Console.WriteLine("Hello {0}, you are {1}.", Username, Age); // Avec placeholder syntax
+            // Attention , écrire {0 1} ne fonctionne pas, il faudra {0} {1}
+
+    }
+}
+
