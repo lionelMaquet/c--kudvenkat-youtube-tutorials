6319d6d14b666dd1c10531b216ec3e8897917cd6
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-06 13:02:09 +0100

    part 9 finished

---
+++ kudvenkat_tutorials/Part_9_Comments_project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Single line comment
+
+        /*
+        MULTI
+        LINE
+        COMMENT 
+         */
+
+        SampleClass MySampleClass = new SampleClass();
+        // -> hover over SampleClass, it shows information because of the XML Documentation Comment below
+    }
+}
+
+// Xml Document comment below 
+/// <summary>
+/// This is a sample class and a sample documentation
+/// </summary>
+public class SampleClass
+{
+}
+
