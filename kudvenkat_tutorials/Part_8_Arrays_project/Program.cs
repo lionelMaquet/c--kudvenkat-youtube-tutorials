d1b4d3621dfb59b19ce14720d1a90cb679efdfd8
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-06 12:52:17 +0100

    Part 8 finished

---
+++ kudvenkat_tutorials/Part_8_Arrays_project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Integer array
+        int[] EvenNumbers = new int[3];
+        EvenNumbers[0] = 0;
+        EvenNumbers[1] = 2;
+        EvenNumbers[2] = 4;
+        //EvenNumbers[2] = "hello"; // Error, array is of type int!
+
+        // An array is of fixed length
+
+        int[] myArray = { 1, 2, 3 }; // Same as saying that the size is 3
+        Console.WriteLine(myArray.Length);
+        // myArray[3] = 4; // Error ! Index out of range.
+    }
+}
+
