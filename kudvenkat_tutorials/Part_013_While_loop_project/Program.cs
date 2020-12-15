85147664e2dce9145a7cf84972290d3b49de8c6c
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-15 17:30:08 +0100

    Part 13 done

---
+++ kudvenkat_tutorials/Part_013_While_loop_project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your target number");
+
+        int UserTarget = int.Parse(Console.ReadLine());
+        int Start = 0;
+
+        while (Start <= UserTarget) // The whole block below is executed UNTIL this expression is evaluated to false. 
+        {
+            Console.WriteLine(Start);
+            Start ++;
+        }
+    }
+}
+
